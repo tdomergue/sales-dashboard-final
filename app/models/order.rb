1604553b16bce83e22e183a4e5cacc554984c3ea
Thibaud Domergue <thibaud.domergue@hei.fr>
2021-05-05 12:00:58 +0200

    destroy Sale model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ApplicationRecord
-  has_many :sales
   belongs_to :customer
   belongs_to :country
   belongs_to :customer
--- app/models/sale.rb
+++
@@ -1,5 +0,0 @@
-class Sale < ApplicationRecord
-  belongs_to :order
-  belongs_to :customer
-  belongs_to :country
-end
